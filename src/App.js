81dd07068399852278b70bac292a38ac2e067b2f
piyush priyadarshi <piyush.p@gmail.co>
2024-02-01 01:43:01 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import { useEffect } from "react";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { useQuery, gql } from "@apollo/client";
+
+
+
 
 function App() {
+
+  const FILMS_QUERY = gql`
+   {
+    getTodo {
+      id,
+      name
+    }
+  }
+`;
+const { data, loading, error } = useQuery(FILMS_QUERY);
+console.log("loding",loading)
+
+useEffect(()=>{
+
+
+},[])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col dark:bg-red-500 bg-gradient-to-r from-teal-400 to-blue-500  h-screen">
+      {/* <div className="flex flex-row h-10  justify-center space-x-4 items-center">
+        <div className="hover:text-green-400">HOME</div>
+        <div className="hover:text-green-400">DEMOS</div>
+        <div className="hover:text-green-400">INNER PAGES</div>
+        <div className="hover:text-green-400">FEATURES</div>
+      </div> */}
+
+      {/* <div className="bg-blue-500 sm:bg-green-500 md:bg-indigo-500 lg:bg-red-500 xl:bg-black h-48">
+        </div> */}
+
+      <div className="h-16">
+
+      <Navbar/>
+
+      </div>
+
+     
+{/* {JSON.stringify((data?.getTodo))} */}
+
+
+{data?.getTodo.map((i)=>{
+
+
+return(
+
+  <p>
+
+    {(i.name)}
+  </p>
+)
+})}
+
+      <div className="h-48 mx-5 rounded-lg flex flex-col sm:flex-col md:flex-row lg:flex-row my-5">
+        <img
+          className="h-48 bg-blue-500 basis-1/3 mx-5 rounded-lg my-5"
+          src={require("./asset/image/Mountain.webp")}
+        />
+        <img
+          className="h-48 bg-green-500 basis-1/3 mx-5 rounded-lg my-5"
+          src={require("./asset/image/Mountain1.jpeg")}
+        />
+        <img
+          className="h-48 bg-orange-500 basis-1/3 mx-5 rounded-lg my-5"
+          src={require("./asset/image/Mountain.webp")}
+        />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+
+const client = new ApolloClient({
+  uri: "http://localhost:8000/graphql",
+  cache: new InMemoryCache()
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+      <ApolloProvider client={client}>
+
     <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
